83c93596d0bb15e7cad1b9f3edcb3c0343998fa1
ThatFinnishGuy <ilari.saarenpaa@gmail.com>
2019-12-15 19:05:05 +0200

    Gameplay Commit
    Added pulsing sun, fading platform and moving platform.

---
+++ Icarus/Assets/Scripts/Background/SunGlow.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunGlow : MonoBehaviour
+{
+    private Material sunMat;
+
+    public float duration;
+
+    void Awake()
+    {
+        sunMat = GetComponent<MeshRenderer>().material;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GlowPulse();
+    }
+
+    void GlowPulse()
+    {
+        float phi = Time.time / duration * 2 * Mathf.PI;
+        float amplitude = Mathf.Cos(phi) * 0.5f + 0.5f;
+
+        sunMat.SetFloat("_GlowScale", amplitude);
+    }
+
+
+}
---
+++ Icarus/Assets/Scripts/Platform/FadingPlatform.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadingPlatform : MonoBehaviour
+{
+    public float fadeTime;
+    private SpriteRenderer spriteRenderer;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FadeSprite();
+    }
+
+    void FadeSprite()
+    {
+        Color spriteColor = spriteRenderer.color;
+        spriteColor.a -= Time.deltaTime * fadeTime;
+
+        spriteRenderer.color = spriteColor;
+
+        if (spriteColor.a <= 0)
+            GameObject.Destroy(gameObject);
+    }
+}
